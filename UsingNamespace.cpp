3d06ba3a8f99232e400fd93a92a5141242fe18fb
Nick Elgar <elgar89@icloud.com>
2023-03-09 11:57:52 +0000

    Added UsingNamespace.cpp to learn using std::

---
+++ UsingNamespace.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+	long long favy_number;
+
+	cout << "What is your favy number bro?" << endl;
+
+	cin >> favy_number;
+
+	cout << "Woah bro! Mine is " << favy_number << " too!" << endl;
+
+	return 0;
+
+}
