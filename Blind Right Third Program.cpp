7453350b48439a3b079bfdc092c05681f546dc1d
Nick Elgar <elgar89@icloud.com>
2023-03-10 20:44:43 +0000

    Added Blind Code Practice

---
+++ Blind Right Third Program.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	long long favynumber;
+
+	cout << "What is your favy num? " << endl;
+
+	cin >> favynumber;
+
+	cout << "Hey bro, that's mad wild bro, my favy num is 1 less than yours: " << endl
+		<< "It's: " << (favynumber - 1) << endl;
+
+	return 0;
+	
+}
