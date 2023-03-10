33eded769827356309eebf17f8182989bc75353b
Nick Elgar <elgar89@icloud.com>
2023-03-10 22:20:41 +0000

    Global & Local Variables - Look at string to char weirdness at some point

---
+++ Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Global Variable
+string dog_noise {'w'};
+
+int main()
+{
+	//Local Variable
+	//string dog_noise{'J'};
+
+	cout << "Dog's go: ";
+		//cin >> dog_noise;
+		cout << dog_noise;
+
+	return 0;
+	
+}
