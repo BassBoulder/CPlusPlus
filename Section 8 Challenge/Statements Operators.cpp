980dc6fe941d64db5d4fc8e2d704d2cb10c6b24e
Nick Elgar <elgar89@icloud.com>
2023-03-19 13:31:48 +0000

    Statements & Operators var++ & ++var logic

---
+++ Section 8 Challenge/Statements Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int cat_count{ 1 };
+
+	cout << cat_count << endl;
+
+	cout << cat_count++ << endl; //increment after variable usage, i.e. variable++
+
+	cout << cat_count;
+
+	return 0;
+	
+}
