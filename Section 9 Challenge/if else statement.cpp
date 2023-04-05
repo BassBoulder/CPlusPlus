a85f4673e88fc90de71bf33a2904e415ec356358
Nick <elgar89@icloud.com>
2023-04-05 17:14:06 +0100

    nested ifs plus if else

--- Section 9 Challenge/if else statement.cpp
+++ Section 9 Challenge/if else statement.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	int num{11};
+	int num{};
 	const int ten{ 10 };
+
+	cin >> num;
 
 	if (num < ten)
 	{
+		if (num == 1)
+			cout << "You're a porky-boy" << endl;
+		if (num == 2)
+			cout << "Chunky says what?" << endl;
+		if (num == 3)
+			cout << "You must want ma'ma's gumbo!" << endl;
+		
 		cout << "Yes" << endl;
 	}
 	else
 	{
-		cout << "Nope" << endl;
+		cout << "Ah fatty bum bum!" << endl;
 	}
 
 	return 0;
