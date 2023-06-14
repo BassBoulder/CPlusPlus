1cc89c1fcf9116b471b3fb581b0fd0395be449ee
Nick <90762430+BassBoulder@users.noreply.github.com>
2023-06-14 20:04:45 +0100

    Updated Section 10 Challenge to decrypt

--- Section 10 Challenge/Section 10 Challenge.cpp
+++ Section 10 Challenge/Section 10 Challenge.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-	string alphabet{"AbcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	string key{"0123456789abcdefghijkABCDEFGHIJKlmnopqrstuvwxyzMNOPQ"};
+	string alphabet{ "AbcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+	string key{ "0123456789abcdefghijkABCDEFGHIJKlmnopqrstuvwxyzMNOPQ" };
 
 	string text{};
 	cout << "What's the message?" << endl;
@@ -31,6 +31,28 @@ int main()
 	}
 
 	cout << encrypted_message;
+
+
+	string decrypted_message{};
+
+	cout << endl << "decrypting message" << endl;
+
+	for (char c : encrypted_message)
+	{
+		size_t position2 = key.find(c);
+		if (position2 != string::npos)
+		{
+			char new_char2{ alphabet.at(position2) };
+			decrypted_message += new_char2;
+
+		}
+		else
+		{
+			decrypted_message += c;
+		}
+	}
+
+	cout << decrypted_message;
 
 	return 0;
 }
