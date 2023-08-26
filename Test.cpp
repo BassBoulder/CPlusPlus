ba5f51175bbf6778ef31a2e53c2820246a2101f6
Nick <90762430+BassBoulder@users.noreply.github.com>
2023-08-26 10:23:40 +0100

    Testing my basic form after a little while away

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+string feeling{};
+
+int main()
+{
+
+	cout << "How are you feeling?" << endl;
+	cin >> feeling;
+	cout << endl << "Esta bien!" << endl;;
+
+	return 0;
+
+}
