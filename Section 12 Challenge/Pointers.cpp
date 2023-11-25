c28297bd91f868a560e8bcc643a17ca1cf18b9bc
Nick <90762430+BassBoulder@users.noreply.github.com>
2023-11-25 08:17:31 +0000

    working on pointers - did it cold nearly perfect (learned from mistakes Nicky Boy!)

--- Section 12 Challenge/Pointers.cpp
+++ Section 12 Challenge/Pointers.cpp
@@ -6,12 +6,19 @@ int main()
 {
 	int cat_number{ 100 };
 
-	cout << cat_number << endl << endl;
+	cout << "Cat Number: " << cat_number << endl << endl;
 
-	int cat_number_pointer{nullptr};
-	cat_number_pointer = *cat_number;
+	int *cat_number_pointer {nullptr};
+	
+	cout << "NULL Pointer: " << cat_number_pointer;
+	
+	cat_number_pointer = &cat_number;
 
-	cout << cat_number_pointer;
+	cout << endl;
+
+	cout << "Address of: " << cat_number_pointer << endl;
+
+	cout << "Dereference Pointer: " << *cat_number_pointer << endl;
 
 	return 0;
-	}
+}
