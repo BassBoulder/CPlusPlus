c3c9b23c3875616c2233ed54945240d7475108eb
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-10-29 11:08:04 +0000

    Completed Log Levels Exercism exercise

---
+++ C++/Exercism/LogLevels.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+namespace log_line {
+    std::string message(std::string line) {
+
+        return line.substr(line.find(":") + 2);
+    }
+
+    std::string log_level(std::string line) {
+
+        return line.substr(1, line.find(":") - 2);
+    }
+
+    std::string reformat(std::string line) {
+
+        return message(line) + " (" + log_level(line) + ")";
+    }
+}
